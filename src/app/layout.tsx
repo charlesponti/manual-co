9aa1cd81bd3342cc45c8de6300a56298e07929c9
Charles Ponti <cj@ponti.io>
2025-03-22 15:49:57 -0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import QueryProvider from "@/providers/query-provider";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+	variable: "--font-manrope",
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	display: "swap",
+});
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Ponti Studios - Symptom Guidance",
+	description: "symptom guidance",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${manrope.className} antialiased`}>
+				<QueryProvider>{children}</QueryProvider>
+			</body>
+		</html>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+	return (
+		<div className="container mx-auto min-h-screen flex flex-col justify-center items-center">
+			🐈‍⬛ meow
+		</div>
+	);
+}
